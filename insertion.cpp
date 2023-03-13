9e54314d8d0b72f48a16a949eda1a425031a22d8
Nafis-2007075 <126339346+Nafis-2007075@users.noreply.github.com>
2023-03-13 21:53:20 +0600

    Add files via upload

---
+++ insertion.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i,j,temp;
+    cin>>n;
+    int a[n];
+    for(i=0 ; i<n ; i++)
+    {
+        cin>>a[i];
+    }
+    for(i=1 ; i<n ; i++)
+    {
+        temp=a[i];
+        j=i-1;
+        while(j>=0 && a[j]>temp)
+        {
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=temp;
+    }
+     for(i=0 ; i<n ; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
