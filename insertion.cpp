d7009dd225dd3d4858f6b700d3ace151e0a8d66f
Nafis-2007075 <126339346+Nafis-2007075@users.noreply.github.com>
2023-03-14 08:20:22 +0600

    Update insertion.cpp

--- insertion.cpp
+++ insertion.cpp
@@ -1,26 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
+void swap(int &a,int &b)
+{
+    a=a+b;
+    b=a-b;
+    a=a-b;
+}
+void inst_sort(int a[],int n)
+{
+    int i,j;
+    for(i=0 ; i<n ; i++)
+    {
+        int temp=a[i];
+        j=i-1;
+        while(j>=0 && a[j]>temp)
+        {
+            swap(a[j+1],a[j]);
+            j--;
+        }
+    }
+}
 int main()
 {
-    int n,i,j,temp;
+    int n,i;
     cin>>n;
     int a[n];
     for(i=0 ; i<n ; i++)
     {
         cin>>a[i];
     }
-    for(i=1 ; i<n ; i++)
-    {
-        temp=a[i];
-        j=i-1;
-        while(j>=0 && a[j]>temp)
-        {
-            a[j+1]=a[j];
-            j--;
-        }
-        a[j+1]=temp;
-    }
-     for(i=0 ; i<n ; i++)
+    inst_sort(a,n);
+    for(i=0 ; i<n ; i++)
     {
         cout<<a[i]<<" ";
     }
